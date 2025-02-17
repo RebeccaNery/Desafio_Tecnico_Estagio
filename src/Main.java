1e07178a549334d39c9d4123324bb36d676525bb
Rebecca Nery <112131032+RebeccaNery@users.noreply.github.com>
2025-02-17 20:55:42 -0300

    método descreveCodigo ok

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,58 @@ public class Main {
         return (i - 1) >= 0 && (i + 1) < stringOriginal.length();
     }
 
+    public static int descreveCodigo(String stringOriginal) {
+        int abertura = 0;
+        int fechamento = 0;
+        int inicioTexto = 0;
+        int fimTexto = 0;
+
+        for (int i = 0; i < stringOriginal.length(); i++) {                 //percorre o codigo html
+            if (verificaLimites(i, stringOriginal)) {                       // SE A POSIÇÃO EXISTE
+                if (stringOriginal.charAt(i) == '<') {                      // SE FOR O SINAL DE MENOR QUE
+
+                    if (stringOriginal.charAt(i - 1) != '>' /*&& stringOriginal.charAt(i - 1) != ' '*/) {
+                        fimTexto = i - 1;
+                        System.out.println("Aqui tem fim de texto. Posição:" + fimTexto);
+
+                    }//FIM DE TEXTO
+
+                    if (stringOriginal.charAt(i+1) == '/'){     //FECHAMENTO DE TAG
+                        fechamento++;
+                        System.out.println("Aqui tem tag de fechamento. Posição:" + i + " / Fechamento:" + fechamento);
+                    } else {                    //SE O PROXIMO NAOOOOO FOR BARRA, QUER DIZER QUE É TAG ABERTURA
+                        abertura++;
+                        System.out.println("Aqui tem tag de abertura. Posição:" + i + " / Abertura:" + abertura);
+                        //ABERTURA DE TAG
+                    }
+
+                    i++;
+
+                } //<<<<<<<<<<<<<<<<<<<<<<
+
+
+                if (stringOriginal.charAt(i) == '>') {                      // SE FOR O SINAL DE MAIOR QUE
+
+                    if (stringOriginal.charAt(i + 1) != '<') {              // OUTRA ABERTURA DE TAG EM SEQUENCIA
+                        inicioTexto = i + 1;
+                        System.out.println("Aqui tem inicio de texto. Posição:" + inicioTexto);
+                        i++;
+                    }                                                           //INICIO DE TEXTO
+
+                } //>>>>>>>>>>>>>>>>>>>>>>
+
+
+            } else if (stringOriginal.charAt(i) == '<') {
+                abertura++;
+                System.out.println("Aqui tem tag de abertura. Posição:" + i + " / Abertura:" + abertura);
+                //primeira tag de abertura
+            } else if (stringOriginal.charAt(i) == '>'){
+                System.out.println("Fim do código. Posição:" + i);     //ultimo caracter
+            }
+        } //for
+return abertura;
+    }//metodo descreveCodigo
+
     public static void main(String[] args) {
 
         String[] trechos = new String[100];
