dfc863d05d887cac42dd2ec4faadc0da5852ac1c
Rebecca Nery <112131032+RebeccaNery@users.noreply.github.com>
2025-02-16 22:37:23 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int j = 0;
-        int nivel = 0;
+        String[] trechos = new String[100];
         String tagAbertura = "<";
-        String tagFechamento = "</";
+        String tagFechamentoParcial = ">";
+        String tagFechamentoTotal = "</";
+        String inicioDeTexto = ">";
+        String fimDeTexto = "<";
+
+        int j = 0;
+        //int nivel = 0;
+        int abertura = 0;
+        int fechamento = 0;
+
         //String stringOriginal = "<html> <head> coisas </head> </html>"; //===> só que a "string original" é um código
         // html!
         String stringOriginal =
@@ -20,33 +28,38 @@ public class Main {
                         "</body> " +
                 "</html>";
 
-        //Encontrando o nível mais profundo
-        for (int i = 0; i < stringOriginal.length(); i++) { //percorre
+        //Encontrando o nível
+        for (int i = 0; i < stringOriginal.length(); i++) { //percorre a string fornecida
+
             if (stringOriginal.charAt(i) == '<' && stringOriginal.charAt(i + 1) != '/') {
-                nivel++;
-                if (stringOriginal.charAt(i) != '/') {
-                    System.out.println("Aqui tem tag." + i);
-                    i++;
-                }else{
-                    System.out.println("Aqui tem tag de fechamento." + i);
-                    i++;
-                }
-            }
-        }
+                abertura++;
+                System.out.println("Aqui tem tag de abertura. Posição:" + i);
+                i++;
+            }//if
+
+            if (stringOriginal.charAt(i) == '<' && stringOriginal.charAt(i + 1) == '/'){
+                fechamento++;
+                System.out.println("Aqui tem tag de fechamento. Posição:" + i);
+                i++;
+            }//if
+
+            trechos[i] = stringOriginal.substring(abertura, fechamento);
+
+        }//for
+
 
         //fazer uma substring entre >AQUI</ para mostrar na tela a frase mais profunda
 
         String resultado = "Hello world!";
-        System.out.println("Nível:" + nivel);
+        //System.out.println("Nível:" + nivel);
         System.out.println(stringOriginal);
         System.out.println(resultado);
 
     }// Metodo main
 
 
-
-
-
-
-
 }// Classe Main
+
+/*
+
+ */
