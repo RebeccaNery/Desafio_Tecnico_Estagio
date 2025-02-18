efd1c9f3ed62151c207c8a3fe100649274e04560
Rebecca Nery <112131032+RebeccaNery@users.noreply.github.com>
2025-02-17 22:06:06 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,6 @@ public class Main {
         return trechos;
     }
 
-
     public static void mostraVetorInt (int[] vetor){
         for (int i=0; i< vetor.length; i++){
             if (vetor[i] != 0){
@@ -35,13 +34,14 @@ public class Main {
         }//for
     }
 
-    //trechos[k] = stringX.substring(abertura, fechamento);
-
     public static int[] encontraPosicoes(String stringOriginal) {
         int abertura = 0;
         int fechamento = 0;
         int inicioTexto = 0;
         int fimTexto = 0;
+        int [] posicoes = new int[stringOriginal.length()];
+        int indicePosicoes = 0;
+        //String[] trechos = new String[100];
 
         for (int i = 0; i < stringOriginal.length(); i++) {                 //percorre o codigo html
             if (verificaLimites(i, stringOriginal)) {                       // SE A POSIÇÃO EXISTE
@@ -50,7 +50,7 @@ public class Main {
                     if (stringOriginal.charAt(i - 1) != '>' /*&& stringOriginal.charAt(i - 1) != ' '*/) {
                         fimTexto = i - 1;
                         System.out.println("Aqui tem fim de texto. Posição:" + fimTexto);
-
+                        posicoes[indicePosicoes++] = fimTexto;
                     }//FIM DE TEXTO
 
                     if (stringOriginal.charAt(i+1) == '/'){     //FECHAMENTO DE TAG
@@ -73,6 +73,7 @@ public class Main {
                         inicioTexto = i + 1;
                         System.out.println("Aqui tem inicio de texto. Posição:" + inicioTexto);
                         i++;
+                        posicoes[indicePosicoes++] = inicioTexto;
                     }                                                           //INICIO DE TEXTO
 
                 } //>>>>>>>>>>>>>>>>>>>>>>
@@ -86,85 +87,42 @@ public class Main {
                 System.out.println("Fim do código. Posição:" + i);     //ultimo caracter
             }
         } //for
-return abertura;
-    }//metodo descreveCodigo
+return posicoes;
+    }//metodo encontraPosicoes
 
     public static void main(String[] args) {
 
-        String[] trechos = new String[100];
-        String tagAbertura = "<";
-        //String tagFechamentoParcial = ">";
-        String tagFechamentoTotal = "</";
-        String inicioDeTexto = ">";
-        String fimDeTexto = "<";
-
-        int j = 0;
-        //int nivel = 0;
-        int abertura = 0;
-        int fechamento = 0;
-        int inicioTexto = 0;
-        int fimTexto = 0;
+//        String tagAbertura = "<";
+//        String tagFechamentoTotal = "</";
+//        String inicioDeTexto = ">";
+//        String fimDeTexto = "<";
 
         //String stringOriginal = "<html> <head> coisas </head> </html>"; //===> só que a "string original" é um código
         // html!
-        String stringOriginal =
-                "<html>" +
-                        "<head>" +
-                            "<title>" +
-                                "Este é o título." +
-                            "</title>" +
-                        "</head>" +
-                        "<body>" +
-                            "Este é o corpo." +
-                        "</body>" +
-                "</html>";
+//        String stringOriginal =
+//                "<html>" +
+//                        "<head>" +
+//                            "<title>" +
+//                                "Este é o título." +
+//                            "</title>" +
+//                        "</head>" +
+//                        "<body>" +
+//                            "Este é o corpo." +
+//                        "</body>" +
+//                "</html>";
 
-        //Encontrando o nível
-        for (int i = 0; i < stringOriginal.length(); i++) { //percorre o codigo html
+        // Passando a String que será analisada.
+        String stringOriginal = "<html><head><title>Este é o título.</title></head><body>Este é o corpo.</body></html>";
 
-            if (stringOriginal.charAt(i) == '<') {                          // SE FOR O SINAL DE MENOR QUE
-                if ((i - 1) >= 0 && (i + 1) < stringOriginal.length()) {                      // SE A POSIÇÃO EXISTE
-                    if (stringOriginal.charAt(i+1) == '/'){     //FECHAMENTO DE TAG
-                        fechamento++;
-                        System.out.println("Aqui tem tag de fechamento. Posição:" + i);
-                        i++;
-                    } else if (stringOriginal.charAt(i - 1) != '>' && stringOriginal.charAt(i - 1) != ' ') {
-                        fimTexto = i - 1;
-                        System.out.println("Aqui tem fim de texto. Posição:" + fimTexto);
-                        i++;                // FIM DE TEXTO
-                    } else if(stringOriginal.charAt(i + 1) != '/') {    //SE O PROXIMO NAOOOOO FOR BARRA, QUER DIZER QUE É TAG ABERTURA
-                            abertura++;
-                            System.out.println("Aqui tem tag de abertura. Posição:" + i);
-                            i++;                //ABERTURA DE TAG
-                    }
-                }else{
-                    System.out.println("Não há caractere antes/depois. Posição :" + i);
-                } //FORA DOS LIMITES
+        // Determinando em quais posições se iniciam e concluem-se os textos.
+        int [] posicoes = encontraPosicoes(stringOriginal);
+        mostraVetorInt(posicoes);
+
+        // Guardando no vetor "trechos" os textos encontrados de acordo com as posições do vetor "posicoes".
+        String[] trechos = encontraTextos(stringOriginal, posicoes);
+        mostraVetorString(trechos);
 
 
-//<<<<<<<<<<<<<<<<<<<<<<
-
-            } else if (stringOriginal.charAt(i) == '>') {                     // SE FOR O SINAL DE MAIOR QUE
-                if ((i - 1) >= 0 && (i + 1) < stringOriginal.length()) {
-                    if (stringOriginal.charAt(i + 1) != '<') {           // OUTRA ABERTURA DE TAG EM SEQUENCIA
-                        inicioTexto = i + 1;
-                        System.out.println("Aqui tem inicio de texto. Posição:" + inicioTexto);
-                        i++;
-                    }                                                           //INICIO DE TEXTO
-
-                } //>>>>>>>>>>>>>>>>>>>>>>
-
-
-            } else if (stringOriginal.charAt(i) == '<') {
-                System.out.println("Aqui tem tag de abertura. Posição:" + i);       //primeira tag de abertura
-            } else if (stringOriginal.charAt(i) == '>'){
-                System.out.println("Fim do código. Posição:" + i);     //ultimo caracter
-            }
-        } //for
-
-                //trechos[i] = stringOriginal.substring(abertura, fechamento);
-
-        }//for
 
 
         //fazer uma substring entre >AQUI</ para mostrar na tela a frase mais profunda
@@ -178,7 +136,3 @@ return abertura;
 
 
 }// Classe Main
-
-/*
-
- */
