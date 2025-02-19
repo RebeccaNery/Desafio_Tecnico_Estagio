4e38cac431e88f9be566bc742f199f16b0194c35
Rebecca Nery <112131032+RebeccaNery@users.noreply.github.com>
2025-02-18 22:10:19 -0300

    novos imports

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
 
